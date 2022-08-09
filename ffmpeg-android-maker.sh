ac7c2f1e961de9facf88e841371a3995309a3d4a
nklhrstv <me@nikolahristov.com>
2022-08-09 16:11:45 +0300

    build static ffmpeg

--- ffmpeg-android-maker.sh
+++ ffmpeg-android-maker.sh
@@ -34,7 +34,7 @@ export BUILD_DIR_EXTERNAL=$BUILD_DIR/external
 function prepareOutput() {
   OUTPUT_LIB=${OUTPUT_DIR}/lib/${ANDROID_ABI}
   mkdir -p ${OUTPUT_LIB}
-  cp ${BUILD_DIR_FFMPEG}/${ANDROID_ABI}/lib/*.so ${OUTPUT_LIB}
+  cp ${BUILD_DIR_FFMPEG}/${ANDROID_ABI}/lib/*.a ${OUTPUT_LIB}
 
   OUTPUT_HEADERS=${OUTPUT_DIR}/include/${ANDROID_ABI}
   mkdir -p ${OUTPUT_HEADERS}
@@ -117,8 +117,6 @@ do
     # Returning to the root directory. Just in case.
     cd ${BASE_DIR}
   done
-
-  checkTextRelocations || exit 1
 
   prepareOutput
 done
